15041d241d6b4cdbcfc43a887e87017b60f480a2
Peter <peter@afosto.com>
2016-10-03 14:01:34 +0200

    Fix for: Fatal error:  Trait 'Afosto\ApiClient\Models\Sales\WebhookTrait' not found in /afosto/vendor/afosto/api-client/src/Models/Sales/Sale.php on line 16

--- src/Models/Sales/Sale.php
+++ src/Models/Sales/Sale.php
@@ -3,6 +3,7 @@
 namespace Afosto\ApiClient\Models\Sales;
 
 use Afosto\ApiClient\Models\_Base\Sales\BaseSale;
+use Afosto\ApiClient\Components\WebhookTrait;
 
 /**
  * Use this class for custom methods that extend the default functionality for 
@@ -11,7 +12,7 @@ use Afosto\ApiClient\Models\_Base\Sales\BaseSale;
  * This class can interact with api, through methods like find and save.
  */
 class Sale extends BaseSale {
-    
+
     use WebhookTrait;
 
     public function getAttributes() {
